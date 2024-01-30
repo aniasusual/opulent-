5c090b133229502d7be7912a09ecbfe43de2f8e7
Animesh <82938725+aniasusual@users.noreply.github.com>
2024-01-30 18:07:05 +0530

    changed the auth code in Backend/middleware/auth

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -3,37 +3,31 @@ const userModel = require('../models/userModel');
 const ErrorHandler = require("../utils/errorHandler");
 
 exports.isAuthenticated = async function (req, res, next) {
-    try {
-        const token = await req.cookies.token;
-        // console.log(token)
-        // console.log(token);
-        // this will return id
-        // if(token)
-        // {
-          const decodedData = jwt.verify(token, process.env.JWT_SECRET);
-          req.user = await userModel.findById(decodedData.id);
-          console.log(req.user);
-        // }
+  const { token } = req.cookies;
 
-        next();
+  if (!token) {
+    return next(new ErrorHandler("Please Login to access this resource", 401));
+  }
 
-    } catch (error) {
-        console.log("loda error occured: ", error.message);
-    }
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await userModel.findById(decodedData.id);
+
+  next();
 }
 
 exports.isAuthorized = (...roles) => {
-    return (req, res, next) => {
-      if (!roles.includes(req.user.role)) {
-        return next(
-          new ErrorHandler(
-            `Role: ${req.user.role} is not allowed to access this resouce `,
-            403
-          )
-        );
-      }
-  
-      next();
-    };
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resouce `,
+          403
+        )
+      );
+    }
+
+    next();
+  };
 
 }
