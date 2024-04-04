c5f639e7b2ec8137b9bc569f3756a5d1b221ff07
Animesh <82938725+aniasusual@users.noreply.github.com>
2024-04-04 22:25:45 +0530

    changes the cors origin

--- Backend/app.js
+++ Backend/app.js
@@ -17,7 +17,8 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 
 app.use(cors({
-    origin: "http://localhost:3000", // Add your frontend URL here
+    // origin: "http://localhost:3000", // Add your frontend URL here
+    origin: "https://660ed9e7987141235b1db971--thunderous-begonia-288545.netlify.app", // Add your frontend URL here
     credentials: true, // Allow cookies to be sent with the request
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
